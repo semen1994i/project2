5cebab62b3b6090153552a29fba78529c8349aba
semen1994i <77006294+semen1994i@users.noreply.github.com>
2021-05-24 17:57:28 +0300

    Update mlb.h

--- tests/mlb.h
+++ tests/mlb.h
@@ -28,12 +28,12 @@ TEST(mlb, negative)
     txt->lines->push_back("qwerty\n");
     txt->lines->push_back("summa\n");
     txt->lines->push_back("pogoda\n");
-    txt->cursor->line_num = -1;
+    txt->cursor->line_num = 3;
     txt->cursor->position = -1;
 
     mlb(txt);
 
-    EXPECT_EQ(txt->cursor->line_num, 0);
+    EXPECT_EQ(txt->cursor->line_num, 3);
     EXPECT_EQ(txt->cursor->position, 0);
 
     free(txt);
